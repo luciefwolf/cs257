e5a4d15a4d43bb281655048dfbf5be5c9580ea58
CS Lab Account <cslab@olin310-05.mathcs.carleton.edu>
2022-09-23 09:33:38 -0500

    Added author and book search tests

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -9,6 +9,7 @@ import unittest
 class BooksDataSourceTester(unittest.TestCase):
     def setUp(self):
         self.data_source = BooksDataSource('books1.csv')
+        self.data_source_tiny = BooksDataSource('tiny.csv')
 
     def tearDown(self):
         pass
@@ -17,6 +18,21 @@ class BooksDataSourceTester(unittest.TestCase):
         authors = self.data_source.authors('Pratchett')
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
+
+    def test_unique_book(self):
+        books = self.data_source.books('Omoo')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Omoo'))
+
+    def test_author_search(self):
+        authors = self.data_source.authors('J')
+        self.assertTrue(len(authors) == 3)
+        self.assertTrue(authors[0] == Author('Austen, Jane'))
+        self.assertTrue(authors[1] == Author('Baldwin, James'))
+        self.assertTrue(authors[2] == Author('Bujold, Lois McMaster'))
+
+    def test_book_search(self):
+        books = self.data_source.books('J')
 
 if __name__ == '__main__':
     unittest.main()
